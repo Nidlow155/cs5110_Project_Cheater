09496ed8407abadc898a4ce129e938a64f4558fb
Joey Carlisle <joey.carlisle4@gmail.com>
2020-04-14 16:23:06 -0600

    touched up everything

--- src/Student.py
+++ src/Student.py
@@ -11,38 +11,37 @@ class Student:
         self.id = id
         self.cheat_level = cheat_level
         self.procrastinate_level = procrastinate_level
-
         self.dueDate = dueDate  # considered to be due at 12:01 AM on the dueDate. For example, if it is due on day 10, they have days 0-9 to work on the assignment
-        self.today = 0
+        self.progress = 0  # a number between 0 and 1 that shows the fraction of the total work already completed
+        self.finished = False
 
         lastDay = dueDate - 1
-        self.progress = 0  # a number between 0 and 1 that shows the fraction of the total work already completed
         error = self.dueDate // 10  # error level of 10% of the total work time
         self.startDay = ((procrastinate_level // 10) * lastDay) + randint(-error, error)   # start day = proportional to their procrastination +/- 10% error level
         if self.startDay >= self.dueDate:  # if start day is after its due
             self.startDay = lastDay
-        if self.startDay < 0:
+        if self.startDay < 0:  # if start day is before first day
             self.startDay = 0
         self.finishDay = randint(self.startDay, lastDay)  # finish randomly between the day they start and the day before due
         self.workPerDay = 1 / (self.finishDay - self.startDay + 1)  # get the work done evenly during each day bewteen due date and finish date
 
-        self.finished = False
-
         self.friends = [] # TODO LATER
 
-    def useDay(self):
-        if self.today >= self.dueDate:
+    def useDay(self, today):
+        if today >= self.dueDate:
             raise Exception("The student can't do anything on (or past) the due date, since it is considered to be due at 12:01 AM on the due date.")
-        if (self.today >= self.startDay and not self.finished):
+        # work
+        if (today >= self.startDay and not self.finished):
             self.progress += self.workPerDay
-        self.potentiallyCheat()
         
+        # check if student is done
         if (abs(self.progress - 1) < .00001):
             self.progress = 1
             self.finished = True
+        
+        # cheat if need be
+        self.potentiallyCheat()
 
-
-        self.today += 1
 
     def potentiallyCheat(self):
         pass
@@ -61,7 +60,7 @@ class Class:
 
     def useDay(self, report=False):
         for student in self.students:
-            student.useDay()
+            student.useDay(self.today)
         if report:
             self.report()
         self.today += 1
